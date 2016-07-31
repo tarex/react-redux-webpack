6e637ce9ed6ad4dfcb0a499f7da2fa162ea5e73a
Tareq Jobayere <tarexyz@gmail.com>
2016-08-01 03:14:29 +0600

    eslint fixing

--- assets/src/js/components/devTools.js
+++ assets/src/js/components/devTools.js
@@ -4,9 +4,11 @@ import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-H"
+  <DockMonitor
+    toggleVisibilityKey="ctrl-H"
     defaultIsVisible={true}
-    changePositionKey="ctrl-Q">
+    changePositionKey="ctrl-Q"
+  >
     <LogMonitor />
   </DockMonitor>
 );
